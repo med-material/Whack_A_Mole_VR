c7fb48670c63b4c8e8e3799982f913a41fe5f717
lucas <lucas.menot@viacesi.fr>
2022-09-20 11:46:46 +0200

    Adding LevelFaderScript

---
+++ Assets/Scripts/LevelFaderScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelFaderScreen : MonoBehaviour
+{
+    private float _fadeDuration = 2f;
+
+    [SerializeField]
+    private GameObject panel;
+
+    private void Start()
+    {
+        FadeToBlack();
+        Invoke("FadeFromBlack", _fadeDuration);
+    }
+    private void FadeToBlack()
+    {
+        //set start color
+        SteamVR_Fade.View(Color.clear, 0f);
+        //set and start fade to
+        SteamVR_Fade.View(Color.black, _fadeDuration);
+        panel.SetActive(false);
+    }
+    private void FadeFromBlack()
+    {
+        //set start color
+        SteamVR_Fade.View(Color.black, 0f);
+        //set and start fade to
+        SteamVR_Fade.View(Color.clear, _fadeDuration);
+        panel.SetActive(true);
+    }
+}
