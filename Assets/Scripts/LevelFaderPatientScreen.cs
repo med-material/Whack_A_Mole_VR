402f09594466ded76ee6b18db1f00fbb8954ae03
lucas <lucas.menot@viacesi.fr>
2022-09-21 11:59:18 +0200

    Syncing with fork

---
+++ Assets/Scripts/LevelFaderPatientScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelFaderPatientScreen : MonoBehaviour
+{
+    // Setting fade effect duration
+    private float _fadeDuration = 2f;
+    // Setting grey color from Unity starting logo
+    private Color32 _color = new Color32(77, 77, 77, 255);
+
+    private void Start()
+    {
+        FadeFromGreyToBlack();
+        Invoke("FadeFromBlackToNormal", _fadeDuration);
+    }
+
+    private void FadeFromGreyToBlack()
+    {
+        //set start color (grey)
+        SteamVR_Fade.View(_color, 0f);
+        //set and start fade to black
+        SteamVR_Fade.View(Color.black, _fadeDuration);
+    }
+
+    private void FadeFromBlackToNormal()
+    {
+        //set start color (black)
+        SteamVR_Fade.View(Color.black, 0f);
+        //set and start fade to normal
+        SteamVR_Fade.View(Color.clear, _fadeDuration);
+    }
+}
---
+++ Assets/Scripts/LevelFaderTherapistScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelFaderTherapistScreen : MonoBehaviour
+{
+    // Setting fade effect duration
+    private float _fadeDuration = 2f;
+    // Setting grey color from Unity starting logo
+    private Color32 _color = new Color32(50,50,50, 255);
+
+    // Variable used to stock the therapist panel
+    [SerializeField]
+    public GameObject therapistPanel;
+
+    private void Start()
+    {
+        //disables therapist panel so it doesn't show on launch
+        therapistPanel.SetActive(false);
+        //fade in then fade out effect
+        FadeFromGreyToBlack();
+        Invoke("FadeFromBlackToNormal", _fadeDuration);
+    }
+
+    private void ActivateTherapistScreen()
+    {
+        therapistPanel.SetActive(true);
+    }
+
+    private void FadeFromGreyToBlack()
+    {
+        //set start color (grey)
+        SteamVR_Fade.Start(_color, 0f);
+        //set and start fade to black
+        SteamVR_Fade.Start(Color.black, _fadeDuration);
+    }
+
+    private void FadeFromBlackToNormal()
+    {
+        //set start color (black)
+        SteamVR_Fade.Start(Color.black, 0f);
+        //set and start fade to normal
+        SteamVR_Fade.Start(Color.clear, _fadeDuration);
+        //enables the therapist after fade out effect
+        Invoke("ActivateTherapistScreen", _fadeDuration);
+    }
+}
