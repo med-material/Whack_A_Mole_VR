730b5444aeed070ae29548fccb82a2e7ad388d1a
Selyan Quesnot <silvablocks@gmail.com>
2025-07-29 14:47:07 +0200

    Add Pointer type selector
    - Prepare for external control of pointer type
    - Expose Pointer controller to get it in PointerTypeSelector

---
+++ Assets/Scripts/Pointers/PointerTypeSelector.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Valve.VR;
+
+/*
+PointerTypeSelector manages which Pointer scripts should be enabled/disabled on the gameObject.
+*/
+public class PointerTypeSelector : MonoBehaviour
+{
+    [Tooltip("DefaultPointerType is not switchable while playing: must be set correctly before running.")]
+    [SerializeField] PointerType defaultPointerType = PointerType.BasicPointer;
+
+    private Dictionary<PointerType, Pointer> pointerList = new Dictionary<PointerType, Pointer>();
+    private SteamVR_Behaviour_Pose behaviour_Pose;
+
+    void Start()
+    {
+        behaviour_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        Pointer[] pointers = GetComponents<Pointer>();
+
+        // Populate the pointerList dictionary with pointers found in the GameObject
+        foreach (Pointer pointer in pointers)
+        {
+            if (Enum.TryParse(pointer.GetType().Name, out PointerType pointerType))
+            {
+                pointerList[pointerType] = pointer;
+            }
+            else
+            {
+                Debug.LogError($"Pointer type {pointer.GetType().Name} is not defined in PointerType enum.");
+            }
+        }
+
+        // Activate the default pointer type at the start
+        ActivatePointer(defaultPointerType);
+    }
+
+    public void ActivatePointer(PointerType pointerType)
+    {
+        // Disable all pointers before activating the selected one
+        pointerList.Values.ToList().ForEach(pointer => pointer.enabled = false);
+
+        // Enable the selected pointer
+        if (pointerList.TryGetValue(pointerType, out Pointer pointer))
+        {
+            pointer.enabled = true;
+            behaviour_Pose.inputSource = pointer.Controller; // Change the tracking device for the pointer
+        }
+        else
+        {
+            Debug.LogError($"Pointer type {pointerType} not found.");
+        }
+    }
+}
+
+
+// Important: Ensure to update the PointerType enum when new pointer type script is added /!\
+public enum PointerType
+{
+    BasicPointer,
+    EMGPointer
+}
