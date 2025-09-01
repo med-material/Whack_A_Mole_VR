43244929bf96ce4e9a843bb9a7cf651a5d3bcfbf
Selyan <silvablocks@gmail.com>
2025-09-01 13:07:35 +0200

    EMGDataProcessor: new script - API to access EMG data of Myo Armband

---
+++ Assets/Thalmic Myo/MyoEMG/EMGDataProcessor.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EMGDataProcessor : MonoBehaviour
+{
+    [SerializeField] private ThalmicMyo thalmicMyo;
+    [SerializeField] private int smoothingWindowSize = 250;
+
+    [SerializeField] private int[] rawEMGData = new int[8];
+    [SerializeField] private float rawAbsAverage;
+    [SerializeField] private float smoothedAbsAverage;
+
+    private Queue<int[]> rawEMGDataBuffer = new Queue<int[]>();
+    private Queue<float> rawAbsAverageBuffer = new Queue<float>();
+
+    private void Start()
+    {
+        Debug.Assert(thalmicMyo != null, "StoreEMG reference is missing in EMGDataExposure.");
+        thalmicMyo._myo.EmgData += onReceiveData;
+    }
+
+    private void onReceiveData(object sender, Thalmic.Myo.EmgDataEventArgs data)
+    {
+        // The EMG Pod 08 is unreadable during the first loop iteration. i.e. In the first loop the emg[] size is 7, not 8
+        if (data.Emg == null || data.Emg.Length != 8)
+        {
+            Debug.LogWarning("EMG Data malformed: " + data.Emg);
+            return;
+        }
+
+        rawEMGData = data.Emg;
+        rawAbsAverage = (float) rawEMGData.Select(x => Mathf.Abs(x)).Average();
+
+        // Update EMG data buffer
+        rawEMGDataBuffer.Enqueue(rawEMGData);
+        rawAbsAverageBuffer.Enqueue(rawAbsAverage);
+        if (rawEMGDataBuffer.Count > smoothingWindowSize)
+        {
+            rawEMGDataBuffer.Dequeue();
+            rawAbsAverageBuffer.Dequeue();
+        }
+
+        smoothedAbsAverage = rawAbsAverageBuffer.Average();
+    }
+
+    // ================ Getters ================
+    public int[] GetRawEMGData() => rawEMGData;
+    public float GetRawAbsAverage() => rawAbsAverage;
+    public float GetSmoothedAbsAverage() => smoothedAbsAverage;
+}
