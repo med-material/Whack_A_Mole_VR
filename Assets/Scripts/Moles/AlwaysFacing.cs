db1a7934d36689f878e8928d6b54f0f807a78592
Silvathor <silvablocks@gmail.com>
2025-12-18 11:59:17 +0100

    Add repeat facing option and update logic in AlwaysFacing
    If an object move, the option allow to continuously refacing it.

--- Assets/Scripts/Moles/AlwaysFacing.cs
+++ Assets/Scripts/Moles/AlwaysFacing.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class AlwaysFacing : MonoBehaviour
 {
-    void Start()
+    [SerializeField] bool reapetly = false;
+
+    void Start() => Face();
+
+    void Update()
+    {
+        if (reapetly) Face();
+    }
+
+    private void Face() 
     {
         // To face the object's forward direction towards the origin, rotate 180 degrees after LookAt
         transform.LookAt(new Vector3(0, .5f, 0));
