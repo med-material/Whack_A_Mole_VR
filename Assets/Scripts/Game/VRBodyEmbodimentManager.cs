b6c644fdf23efee964f9da904f1b88f7c2fe32cc
Draxarta <153850638+Draxarta@users.noreply.github.com>
2026-06-10 14:50:37 +0200

    Character sitting
    Adjusted camera and model position to make the character "sit" instead of standing up or flexing his legs to match the sitting position of the player.

--- Assets/Scripts/Game/VRBodyEmbodimentManager.cs
+++ Assets/Scripts/Game/VRBodyEmbodimentManager.cs
@@ -4,6 +4,10 @@ using UnityEngine.InputSystem;
 public class VRBodyEmbodimentManager : MonoBehaviour
 {
     private ModifiersManager.Embodiment embodiment = ModifiersManager.Embodiment.RightHand;
+
+    [SerializeField]
+    private GameObject sittingBench;
+
     public void SetEmbodiment(ModifiersManager.Embodiment selectedEmbodiment)
     {
         this.embodiment = selectedEmbodiment;
@@ -16,33 +20,36 @@ public class VRBodyEmbodimentManager : MonoBehaviour
 
         foreach (Transform mesh in new Transform[] { full, leftArm, rightArm, leftHand, rightHand })
         {
-            if (mesh != null) mesh.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = false;
+            mesh.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = false;
         }
+
+        sittingBench.GetComponent<MeshRenderer>().enabled = false;
 
         switch (embodiment)
         {
             case ModifiersManager.Embodiment.Full:
-                if (full != null) full.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                full.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                sittingBench.GetComponent<MeshRenderer>().enabled = true;
                 break;
             case ModifiersManager.Embodiment.Arms:
-                if (leftArm != null) leftArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
-                if (rightArm != null) rightArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                leftArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                rightArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 break;
             case ModifiersManager.Embodiment.RightArm:
-                if (rightArm != null) rightArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                rightArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 break;
             case ModifiersManager.Embodiment.LeftArm:
-                if (leftArm != null) leftArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                leftArm.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 break;
             case ModifiersManager.Embodiment.Hands:
-                if (leftHand != null) leftHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
-                if (rightHand != null) rightHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                leftHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                rightHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 break;
             case ModifiersManager.Embodiment.RightHand:
-                if (rightHand != null) rightHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                rightHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 break;
             case ModifiersManager.Embodiment.LeftHand:
-                if (leftHand != null) leftHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                leftHand.gameObject.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 break;
         }
     }
