b6755e24dd82a5f55c0c333f8d6242e1ebb9923b
Draxarta <153850638+Draxarta@users.noreply.github.com>
2026-06-26 15:18:50 +0200

    Embodiment | Added Body
    Added body to the playable scene and follows the movement of the player

---
+++ Assets/VR Body/IKFootLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKFootLock : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 lockedPosition;
+
+    private Transform self;
+
+    void Awake()
+    {
+        self = transform;
+    }
+
+    // Update the position of the foot to always stay at the locked position
+    void Update()
+    {
+        self.position = lockedPosition;
+    }
+}
